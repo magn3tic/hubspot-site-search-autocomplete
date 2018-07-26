b822f44d80355a3d2480bd57342fa3f97563e2ba
Colin McGraw <colin@mag.cr>
2018-07-26 16:24:18 -0700

    🙌 initial commit

---
+++ gulpfile.js
@@ -0,0 +1,96 @@
+const gulp = require('gulp');
+const gutil = require('gulp-util');
+const _if = require('gulp-if');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+const uglify = require('gulp-uglify');
+const autoprefixer = require('gulp-autoprefixer');
+const browsersync = require('browser-sync').create();
+
+// gulp --build (publish-ready)
+const argv = require('yargs').argv;
+const isBuild = !!(argv.build);
+
+// demo project paths
+const PATHS = {
+  server: 'demo',
+  inject: 'demo/assets/css/*.css',
+  scss: 'src/scss/*.scss',
+  js: 'src/js/*.js',
+  html: 'demo/*.html',
+  scssIncludes: [],
+  dest: {
+    css: 'demo/assets/css',
+    js: 'demo/assets/js'
+  }
+};
+
+// browserlist target
+const targetBrowsers = ['> 0.25%', 'last 5 versions'];
+
+//webpack config
+const WEBPACK_CONFIG = {
+  context: __dirname+'/src/js',
+  entry: './_index.js',
+  output: {
+    path: isBuild ? __dirname : __dirname+'/demo/assets/js',
+    filename: isBuild ? 'hubspot-autocomplete.min.js' : 'hubspot-autocomplete.js'
+  },
+  bail: false,
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      exclude: /(node_modules|demo)/,
+      loader: 'babel-loader',
+      query: {
+        presets: [['env', {"targets": {"browsers": targetBrowsers }}]]
+      }
+    }]
+  }
+};
+
+//* TASKS START ---------------------------------------------*//
+
+// *TASK - demo/dev server
+gulp.task('server', () => {
+  browsersync.init({
+    server: PATHS.server,
+    files: PATHS.inject,
+    logFileChanges: false,
+    ghostMode: false
+  }, (err, bs) => {});
+});
+
+// *TASK - scss stylesheets
+gulp.task('scss', () => {
+  return gulp.src(PATHS.scss)
+    .pipe(_if(!isBuild, sourcemaps.init()))
+    .pipe(sass({
+      outputStyle: !isBuild ? 'nested' : 'compressed',
+      includePaths: PATHS.scssincludes
+    }).on('error', sass.logError))
+    .pipe(autoprefixer({
+      browsers: targetBrowsers
+    }))
+    .pipe(_if(!isBuild, sourcemaps.write('./')))
+    .pipe(gulp.dest(PATHS.dest.css));
+});
+
+// *TASK - scripts
+gulp.task('js', () => {
+  return gulp.src(PATHS.js)
+    //todo - uglify, browserify, webpackify, whateverify
+    .pipe(uglify())
+    .pipe(gulp.dest(PATHS.dest.js));
+});
+
+// *TASK - webpack (if needed)
+gulp.task('webpack', () => {});
+
+// *TASK - default
+gulp.task('default', ['server', 'scss', 'js'], () => {
+  if (isBuild) {return;}
+  gulp.watch(PATHS.scss, ['scss']);
+  gulp.watch(PATHS.js, ['js']);
+  gulp.watch([PATHS.dest.js+'/*.js', PATHS.html]).on('change', browsersync.reload);
+});
---
+++ src/js/hubspot-autocomplete.js
@@ -0,0 +1,251 @@
+;(function($, win, doc) {
+
+  $.fn.hubspotAutocomplete = function(options) {
+    var defaults = {
+      portalId: '',
+      accentColor: '#226db7',
+      domains: [],
+      pathPrefix: null,
+      pageTypes: ['SITE_PAGE', 'BLOG_POST', 'LANDING_PAGE'],
+      hubDb: {
+        table: null,
+        query: null
+      },
+      truncateContent: false,
+      minInputValue: 0,
+      resultLimit: 15,
+      viewAllLink: 'https://www.culturedstone.com/search?q=%term%',
+      viewAllText: 'View all matches for "%term%"',
+      viewAllIcon: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="9 18 15 12 9 6"></polyline></svg>',
+      noMatchesText: 'No results found matching "%term%"',
+      label: false,
+      input: {
+        type: 'text',
+        autocomplete: 'off',
+        placeholder: 'Search This Site',
+        value: ''
+      },
+      searchTimeout: 300,
+      searchIcon: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>',
+      css: true,
+      enableLoadMore: false
+    },
+    settings = $.extend(true, {}, defaults, options);
+
+    if (!settings.portalId) {
+      alert('There is no Portal ID set for the hubspot search module. Enter that required field and your search bar will be working.');
+      console.error('No PortalId option provided to the hubspot autocomplete search.');
+      return false;
+    }
+
+    var $body = $('body'),
+        currentXHR = null,
+        debounceTimeout = null,
+        searchRestarted = true,
+        lastTerm = null, //reqOpts.term is currentTerm
+        reqUrl = 'https://api.hubapi.com/contentsearch/v2/search?',
+        reqOpts = {
+          portalId: settings.portalId,
+          autocomplete: true,
+          term: null,
+          limit: settings.resultLimit
+        };
+
+    function augmentReqOpts() {
+      if (settings.domains.length > 0) {
+        reqOpts.domain = settings.domains.join(',');
+      }
+      if (settings.hubDb && settings.hubDb.table) {
+        reqOpts.tableId = settings.hubDb.table;
+      }
+      if (settings.pathPrefix) {
+
+      }
+    };
+
+    function getReqUrlParams(obj) {
+      var params = $.params(obj);
+      for (var i=0; i < settings.pageType.length; i++) {
+        params += '&type='+settings.pageType[i];
+      }
+    };
+
+    //style customization
+    function hexToRgb(hex) {
+      var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+      return result ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16)
+      } : null;
+    };
+    function getRgba(rgbobj, opacity) {
+      return 'rgba('+rgbobj.r+', '+rgbobj.g+', '+rgbobj.g+', '+opacity+')';
+    };
+    function writeStylesToHead(css) {
+      var head = doc.head || doc.getElementsByTagName('head')[0],
+          style = doc.createElement('style');
+      style.appendChild(doc.createTextNode(css));
+      head.appendChild(style);
+    };
+    function doColorStyles(color) {
+      var css = '.hssa-input-wrapper input[type="search"]:focus ~ .hssa-input--icon svg, '; 
+      css += '.hssa-input-wrapper input[type="text"]:focus ~ .hssa-input--icon svg,';
+      css+=  '.hssa-results .hssa-viewall a:hover svg {stroke:'+color+'}';
+      css += '.hssa-loader--icon span { background:'+color+'; }';
+      css += '.hssa-results .hs-search-highlight { background:'+getRgba(hexToRgb(color), 0.25)+' }';
+      writeStylesToHead(css);
+    };
+
+    function stopEventPropagation(event) {
+      event.stopPropagation();
+    };
+    function onSearchClickout() {
+      $('.hssa-results-outer.is-focused').removeClass('is-focused');
+    };
+    function abortExistingXhr() {
+      if (currentXHR !== null) { currentXHR.abort(); }
+    };
+    function cancelExistingTimeout() {
+      if (debounceTimeout) { clearTimeout(debounceTimeout); }
+    };
+    function stringReplaceTerm(term) {
+      return term.replace('%term%', reqOpts.term);
+    };
+    function getInputHtml(index) {
+      var input = '<input id="hssa-input-'+index+'" ';
+      for (var key in settings.input) {
+        input += key+'='+'"'+settings.input[key]+'" ';
+      }
+      return input += '>';
+    };
+    function getResultContainers() {
+      var $outer = $(this).parent().next(),
+          $inner = $outer.find('.hssa-results');
+      return { $outer: $outer, $inner: $inner };
+    };
+
+    function getResultsHtml(results) {
+      var html = '<ul>';
+      for (var i=0; i < results.length; i++) {
+        var desc = results[i].description ? '<p><span>'+results[i].description+'<span></p>' : '';
+        html += '<li><a href="'+results[i].url+'"><h4>'+results[i].title+'</h4>'+desc+'</a></li>';
+      }
+      if (settings.viewAllLink) {
+        html += '<li class="hssa-viewall"><a href="'+stringReplaceTerm(settings.viewAllLink)+'"><h4>';
+        html += stringReplaceTerm(settings.viewAllText)+' <span class="hssa-viewall--icon">'+settings.viewAllIcon+'</span></h4></a></li>';
+      }
+      html += '</ul>';
+      return html;
+    };
+
+    function toggleResultsShowing($container, $inner) {
+      if (reqOpts.term.length === 0) {
+        $container.removeClass('has-results has-empty-results is-loading');
+        $inner.html('');
+      } else {
+        $container.addClass('is-loading');
+      }
+    };
+
+    // ajax callbacks
+    function XHRcallbacks($container, $outer) {
+      function onXhrDone(data) {
+        if (data.total > 0) {
+          $container.html(getResultsHtml(data.results));
+          $outer.removeClass('has-empty-results').addClass('has-results');
+        
+        } else {
+          $container.html('<p class="hssa-nomatches">'+stringReplaceTerm(settings.noMatchesText)+'</p>');
+          $outer.removeClass('has-results').addClass('has-empty-results');
+        }
+        $outer.removeClass('is-loading').trigger('hssa.xhrcomplete', [data]);
+      };
+      function onXhrFailure(jqXhr, textStatus, error) {
+        if (textStatus === 'abort') { return; } //ignore timeout based abort
+        $outer.addClass('has-error').trigger('hssa.xhrfail');
+      };
+      return { done: onXhrDone, fail: onXhrFailure };
+    };
+
+    //create new ajax req
+    function XHRcreate($container, $outer) {
+      var xhrcb = XHRcallbacks($container, $outer);
+      currentXHR = $.getJSON(reqUrl + getReqUrlParams(reqOpts));
+      currentXHR.done(xhrcb.done);
+      currentXHR.fail(xhrcb.fail);
+    }
+
+    // auto-search on input
+    function onSearchInput() {
+      abortExistingXhr();
+      cancelExistingTimeout();
+      var $t = $(this),
+          $results = getResultContainers.call(this);
+      reqOpts.term = $.trim($t.val());
+      toggleResultsShowing($results.$outer, $results.$inner);
+      if (reqOpts.term.length > settings.minInputValue && reqOpts.term !== lastTerm) {
+        debounceTimeout = window.setTimeout(function() {
+          XHRcreate($results.$inner, $results.$outer);
+        }, settings.searchTimeout);
+      }
+    };
+
+    // on input focus
+    function onSearchFocus() {
+      var $results = getResultContainers.call(this);
+      $results.$outer.addClass('is-focused');
+    };
+
+    // ui event handlers
+    function bindUiHandlers($box, $input) {
+      $input.on({ input: onSearchInput, focus: onSearchFocus });
+      $box.trigger('hssa.ready').on('click', stopEventPropagation);
+      $(document).on('click', onSearchClickout);
+    };
+
+    //build search box
+    function initialize(index) {
+      var $this = $(this),
+          hasInitialized = $this.hasClass('has-initialized');
+
+      //catch cases where external script gets run multi times
+      if (hasInitialized) { return false; };
+
+      //theme decorative css things
+      if (settings.css) {
+        doColorStyles(settings.accentColor);
+      }
+
+      //build-up & append search box html
+      var $foundInput = $this.find('input[type="text"], input[type="search"]'),
+          hasInput = !!($foundInput.length),
+          $input = hasInput ? $foundInput : $(getInputHtml(index)),
+          $results = $('<div class="hssa-results"></div>'),
+          $loader = $('<div class="hssa-loader"></div>'),
+          loaderIcon = '<div class="hssa-loader--icon"><span></span><span></span><span></span></div>';
+
+      (!hasInput ? $this.prepend($input, $results) : $this.append($results));
+
+      $loader.html(loaderIcon);
+      $results.wrap('<div class="hssa-results-outer"></div>').before($loader);
+      $input.wrap('<div class="hssa-input-wrapper"></div>');
+      if (settings.searchIcon) {
+        $input.after('<span class="hssa-input--icon">'+settings.searchIcon+'</span>')
+              .after('<span class="hssa-input-deco" style="background:'+settings.accentColor+';"></span>');
+      }
+      if (settings.label) {
+        $this.prepend('<div class="hssa-input-label"><label for="hssa-input-'+index+'">'+settings.label+'</label></div>');
+      }
+      if (settings.truncateContent) {
+        $this.addClass('hssa-truncate-content');
+      }
+      $this.addClass('has-initialized');
+
+      bindUiHandlers($this, $input);
+    };
+
+    return $(this).each(initialize);
+  };
+
+})(jQuery, window, document);
