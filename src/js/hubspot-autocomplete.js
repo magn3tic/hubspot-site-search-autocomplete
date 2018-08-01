c1f4ffed654f3fbbbc6e197c0f42d4a05789aeed
Colin McGraw <colin@mag.cr>
2018-08-01 13:41:00 -0700

    viewAllLink default

--- src/js/hubspot-autocomplete.js
+++ src/js/hubspot-autocomplete.js
@@ -24,7 +24,7 @@
       truncateContent: false,
       minInputValue: 0,
       resultLimit: 15,
-      viewAllLink: 'https://www.culturedstone.com/search?q=%term%',
+      viewAllLink: 'https://www.google.com/search?q=%term%',
       viewAllText: 'View all matches for %term%',
       viewAllIcon: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="9 18 15 12 9 6"></polyline></svg>',
       noMatchesText: 'No results found matching "%term%"',
